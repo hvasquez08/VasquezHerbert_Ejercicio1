a0c186945c846bb256ac124d1a5ed7cbd7d32ada
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-02-22 16:50:12 -0600

    ingreso de datos

--- VasquezHerbert_Ejercicio1/Program.cs
+++ VasquezHerbert_Ejercicio1/Program.cs
@@ -1,3 +1,15 @@
 ﻿//incio del trabajo 
 //prioridad medica
-Console.WriteLine("");
+Console.WriteLine("Ingrese el tipo de atencion que necesita");
+Console.WriteLine("1 Emergencia\r\n2 Consulta\r\n3 Pediatría\r\n4 Traumatología");
+int tipo = int .Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la edad del paciente");
+int edad = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la temperatura del paciente en grados centigrados");
+double temperatura = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese saturacin del oxigeno");
+double oxigeno = double.Parse(Console.ReadLine());
+Console.WriteLine("Nivel de dolor(0-10)");
+int dolor = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese presion sistolica");
+double precion  = double.Parse(Console.ReadLine());
