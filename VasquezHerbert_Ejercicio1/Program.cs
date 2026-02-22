eb4fd45679a753d69a657403a728979bf239e106
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-02-22 16:55:48 -0600

    creacion de swich

--- VasquezHerbert_Ejercicio1/Program.cs
+++ VasquezHerbert_Ejercicio1/Program.cs
@@ -1,5 +1,7 @@
 ﻿//incio del trabajo 
 //prioridad medica
+using System.Runtime.CompilerServices;
+
 Console.WriteLine("Ingrese el tipo de atencion que necesita");
 Console.WriteLine("1 Emergencia\r\n2 Consulta\r\n3 Pediatría\r\n4 Traumatología");
 int tipo = int .Parse(Console.ReadLine());
@@ -15,7 +17,17 @@ Console.WriteLine("Ingrese presion sistolica");
 double precion  = double.Parse(Console.ReadLine());
 if (tipo > 0 && edad > 0 && temperatura > 0 && oxigeno > 0 && dolor > 0 && precion > 0)
 {
-
+    switch (tipo)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+            break;
+    }
 }
 else
 {
